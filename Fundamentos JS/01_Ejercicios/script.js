6b3a64c906cac9c945538ca37de6d2b9c1d6a0d3
TheKasumi <bastianvillar@liceovvh.cl>
2024-03-15 10:56:37 -0300

    ok

---
+++ Fundamentos JS/01_Ejercicios/script.js
@@ -0,0 +1,18 @@
+//Ejercicios creacion de variables
+
+//Una variable global de tipo
+var global = "Global";
+//Variable loacl
+let local = "Local";
+//Variable constante
+const constante = "Constante";
+
+
+console.log("Variables: Global: " + global + " Local: " + local + " Constante: " + constante)
+
+let numero = 4; // Número entero
+let decimal = 3.1416; // Número decimal 
+let string = "Texto"; // Cadena de texto
+let boolean = true; // Booleano
+let array = ["Perro", 20, "Gato", 50]; // Array
+let object = {}
