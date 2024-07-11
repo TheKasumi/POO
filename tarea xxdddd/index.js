0ecf34d00a1ac237ca44a103c18bb87354cdb64a
TheKasumi <bastianvillar@liceovvh.cl>
2024-07-11 14:47:10 -0400

    ok

---
+++ tarea xxdddd/index.js
@@ -0,0 +1,4 @@
+let nombres = [];
+function agregarNombre(){
+    let 
+}
--- this usuarios/script.js
+++ this usuarios/script.js
@@ -1,21 +1,20 @@
 const nombreInput = document.getElementById('nombreInput');
 const agregarBtn = document.getElementById('agregarBtn');
 const listaNombres = document.getElementById('listaNombres');
-
+let nombreCount = 0;
 
 agregarBtn.addEventListener('click', function() {
-
     const nombre = nombreInput.value.trim();
 
-    if (nombre !== '') {
+    if (nombre !== '' && nombreCount < 100) {
+        const nuevoNombre = document.createElement('li');
+        nuevoNombre.textContent = nombre;
+        listaNombres.appendChild(nuevoNombre);
+        nombreInput.value = '';
+        nombreCount++;
 
-    const nuevoNombre = document.createElement('li');
-    nuevoNombre.textContent = nombre;
-
-    listaNombres.appendChild(nuevoNombre);
-
-    nombreInput.value = '';
-
-    agregarBtn.disabled = true;
-}
+        if (nombreCount === 100) {
+            agregarBtn.disabled = true;
+        }
+    }
 });
