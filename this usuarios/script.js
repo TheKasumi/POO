76ec8e1f5afe15c5d17cdea7dd42114eaceb7b4c
TheKasumi <bastianvillar@liceovvh.cl>
2024-07-11 12:48:24 -0400

    OK

--- this usuarios/script.js
+++ this usuarios/script.js
@@ -1,12 +1,21 @@
-function addUsuario(){
-    var nameInput = document.getElementById("name")
-    var nameList = document.getElementById("nameList")
+const nombreInput = document.getElementById('nombreInput');
+const agregarBtn = document.getElementById('agregarBtn');
+const listaNombres = document.getElementById('listaNombres');
 
-    if(name.Input.value.trim() !==""){
-        var newName = document.getElementById("Li")
-        newName.textContent = nameInput.value;
-        nameList.appendChild(newName);
 
-        
-    }
+agregarBtn.addEventListener('click', function() {
+
+    const nombre = nombreInput.value.trim();
+
+    if (nombre !== '') {
+
+    const nuevoNombre = document.createElement('li');
+    nuevoNombre.textContent = nombre;
+
+    listaNombres.appendChild(nuevoNombre);
+
+    nombreInput.value = '';
+
+    agregarBtn.disabled = true;
 }
+});
