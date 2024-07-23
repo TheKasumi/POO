b5dc8799e897022aa2b39ddbde11eab7b6764475
ElOtaku001 <felipecastro@liceovvh.cl>
2024-07-23 09:31:04 -0400

    OK

---
+++ prueba/index.js
@@ -0,0 +1,11 @@
+function EncontrarMayor(numeros){
+    let Mayor = numeros[0]; 
+
+    for(let i = 1; numeros.lenght; i++){
+        Mayor = numeros[i]
+
+    }
+    document.getElementById("resultado").innerHTML = Mayor;
+    return 0;
+
+}
